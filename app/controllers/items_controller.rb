81fab17e6710be7097b612f50a95261df0c18abb
kaishinji <bu-meransnake@ezweb.ne.jp>
2019-02-09 15:49:34 +0900

    一覧ページ作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,13 @@
 class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
-    # Itemモデルのfindメソッドを使用して、パラメータの値に一致するidのデータを検索している
-    # 取得したデータを@itemというインスタンス変数にセットする
+    
     
   end
+
+  def index
+  @items = Item.all
+
+  end
+
 end
